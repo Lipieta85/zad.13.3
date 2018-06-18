170c412412fc68710b11e1dc2da1c892e0c11cba
Rafał Lipieta <cwgflojdid@wp.pl>
2018-06-18 18:33:41 +0200

    node.js environment

---
+++ program.js
@@ -0,0 +1,21 @@
+process.stdin.on('readable', function() {
+    
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+        case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+        case '/version':
+            console.log(process.versions.node);
+            break;
+        case '/lang':
+            console.log(process.env.lang);
+            break;
+        default: 
+            process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
